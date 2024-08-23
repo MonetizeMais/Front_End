65e6c82215e30581badd013c71b22039d18e0d03
Nicolas Granatelli Sanches <nicolassanches-ieg@instituto.ad>
2024-08-23 07:43:14 -0300

    Login 70

---
+++ src/Screens/Comecar.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import logo from '.././Assets/LogoPorco.png'; 
+import '.././App.css'; 
+import MainButton from '../components/button.jsx'; 
+import Typical from 'react-typical'
+
+function Comecar() {
+  return (
+    <div className='Comecar'>
+         <div className='box2'>
+
+        <div className='Dialog'>
+            <Typical
+                loop={Infinity}
+                steps={
+                    ['Oie, @Usuario! Sou o Poupinho!', 400]
+                }
+            />
+        </div>
+
+        <img className='LogoPrincipal' src={logo} alt="Logo do Porco" />
+        <MainButton text={'Continuar'} url={'/Perguntas'}/>
+         </div>
+    </div>
+  );
+}
+
+export default Comecar;
--- src/Screens/Routes.jsx
+++ src/Screens/Routes.jsx
@@ -5,6 +5,8 @@ import CadastroDados from '../Screens/CadastroDados';
 import RedefinirSenha from '../Screens/RedefinirSenha';
 import ValidaCodigo from '../Screens/ValidaCodigo';
 import NovaSenha from '../Screens/NovaSenha';
+import Comecar from '../Screens/Comecar';
+import Perguntas from '../Screens/Perguntas';
 
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
@@ -18,6 +20,8 @@ const AppRoutes = () => {
                 <Route path="/RedefinirSenha" element={<RedefinirSenha/>}></Route>
                 <Route path="/ValidaCodigo" element={<ValidaCodigo/>}></Route>
                 <Route path="/NovaSenha" element={<NovaSenha/>}></Route>
+                <Route path="/Comecar" element={<Comecar/>}></Route>
+                <Route path="/Perguntas" element={<Perguntas/>}></Route>
             </Routes>
         </Router>
     )

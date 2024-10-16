be893bea69149d1985829c8dd73db1cb68fb4b63
Nicolas Granatelli Sanches <nicolassanches-ieg@instituto.ad>
2024-10-16 09:19:53 -0300

    subindo

--- src/Screens/Perfil/Perfil.jsx
+++ src/Screens/Perfil/Perfil.jsx
@@ -2,35 +2,42 @@ import React from 'react';
 import './Perfil.css'; 
 import MenuBar from '../../components/MenuBar/MenuBar';
 import logo from '../../Assets/Mascote 2.png';
+import Engrenagem from '../../Assets/Engrenagem.png';
 
 function Personalizacao() {
   return (
     <div>
       <header className="perfil-header">
-        <div className="perfil-code">
-          <img 
-            src={logo} 
-            alt="Avatar do usuário" 
-            className="perfil-avatar"
-          />
-          <div className="perfil-info">
-            <p className="perfil-usuario">usuário</p>
-            <p className="perfil-handle">@usuario</p>
+        <div className='box1'>
+          <div className="perfil-code">
+            <img
+              src={logo}
+              alt="Avatar do usuário"
+              className="perfil-avatar"
+            />
+            <div className="perfil-info">
+              <p className="perfil-usuario">usuário</p>
+              <p className="perfil-handle">@usuario</p>
+            </div>
           </div>
+          <button className="perfil-settings">
+          <img
+              src={Engrenagem}
+              alt="Avatar do usuário"
+              className="engrenagem"
+            />
+          </button>
         </div>
-        <button className="perfil-settings">
-          <i className="icon-settings" />
-        </button>
       </header>
 
       <div className="perfil-container">
         <div className="conteudo">
           <div className="perfil-seguidores">
-            <div className="seguidores-info">
+            <div className="status-info">
               <p>0</p>
               <p>Seguidores</p>
             </div>
-            <div className="seguindo-info">
+            <div className="status-info">
               <p>0</p>
               <p>Seguindo</p>
             </div>
@@ -39,8 +46,17 @@ function Personalizacao() {
           <button className="btn-adicionar-amigos">ADICIONAR AMIGOS</button>
 
           <div className="perfil-completar">
-            <p>Complete o seu perfil</p>
-            <p>Continue personalizando o seu perfil</p>
+          <div className="box2">
+          <img 
+            src={logo} 
+            alt="Avatar do usuário" 
+            className="perfil-image"
+            />
+            <div className='conteudo-popup'>
+              <h3>Complete o seu perfil</h3>
+              <p>Continue personalizando o seu perfil</p>
+            </div>
+          </div>  
             <button className="btn-comecar">COMEÇAR</button>
           </div>
         </div>

a2f747b89f48dbca2d9c6881b0d3a4aad3566ffa
Izabela Adail Paiva Martins <izabelamartins-ieg@instituto.ad>
2024-10-16 07:14:25 -0300

    Subindo tela Personalizacao

--- src/Screens/Routes.jsx
+++ src/Screens/Routes.jsx
@@ -14,6 +14,7 @@ import Pergunta3 from './Pergunta3/Pergunta3';
 import Pergunta4 from './Pergunta4/Pergunta4';
 import Personalizacao from './Personalizacao/Personalizacao';
 import Finalizar from './Finalizar/Finalizar';
+import TelaPersonalizacao from './TelaPersonalizacao/telaPersonalizacao'
 
 import { BrowserRouter as Router, Routes, Route, useLocation } from "react-router-dom";
 // import { CSSTransition, TransitionGroup } from 'react-transition-group';
@@ -41,6 +42,7 @@ const AppRoutes = () => {
                     <Route path="/Pergunta4" element={<Pergunta4 />} />
                     <Route path="/Personalizacao" element={<Personalizacao />} />
                     <Route path="/Finalizar" element={<Finalizar />} />
+                    <Route path="/TelaPersonalizacao" element={<TelaPersonalizacao />} />
                 </Routes>
         //     </CSSTransition>
         // </TransitionGroup>

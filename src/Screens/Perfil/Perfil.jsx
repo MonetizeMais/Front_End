c007010a37edc43549a6121f26410190863931d5
Nicolas Granatelli Sanches <nicolassanches-ieg@instituto.ad>
2024-10-25 08:55:18 -0300

    subindo

--- src/Screens/Perfil/Perfil.jsx
+++ src/Screens/Perfil/Perfil.jsx
@@ -1,68 +1,35 @@
+// Personalizacao.js
 import React from 'react';
-import './Perfil.css'; 
+import './Perfil.css';
 import MenuBar from '../../components/MenuBar/MenuBar';
 import logo from '../../Assets/Mascote 2.png';
-import Engrenagem from '../../Assets/Engrenagem.png';
+import relogio from '../../Assets/relogio (3) 1.png';
+import cadeado from '../../Assets/cadeado 1.png';
+import config from '../../Assets/configuracoes (2) 1.png';
+import logout from '../../Assets/sair-do-usuario (1) 1.png';
+import PerfilOption from '../../components/PerfilOption/PerfilOption';
 
 function Personalizacao() {
   return (
-    <div>
-      <header className="perfil-header">
-        <div className='box1'>
-          <div className="perfil-code">
-            <img
-              src={logo}
-              alt="Avatar do usuário"
-              className="perfil-avatar"
-            />
-            <div className="perfil-info">
-              <p className="perfil-usuario">usuário</p>
-              <p className="perfil-handle">@usuario</p>
-            </div>
-          </div>
-          <button className="perfil-settings">
-          <img
-              src={Engrenagem}
-              alt="Avatar do usuário"
-              className="engrenagem"
-            />
-          </button>
+    <div className="perfil-container">
+      <h1 className="perfil-title">Profile</h1>
+      <div className="perfil-code">
+        <img src={logo} alt="Avatar do usuário" className="perfil-avatar" />
+        <div className="perfil-info">
+          <p className="perfil-usuario">Robi</p>
+          <p className="perfil-handle">8967452743</p>
+          <p className="perfil-email">robi123@gmail.com</p>
         </div>
-      </header>
-
-      <div className="perfil-container">
-        <div className="conteudo">
-          <div className="perfil-seguidores">
-            <div className="status-info">
-              <p>0</p>
-              <p>Seguidores</p>
-            </div>
-            <div className="status-info">
-              <p>0</p>
-              <p>Seguindo</p>
-            </div>
-          </div>
-
-          <button className="btn-adicionar-amigos">ADICIONAR AMIGOS</button>
-
-          <div className="perfil-completar">
-          <div className="box2">
-          <img 
-            src={logo} 
-            alt="Avatar do usuário" 
-            className="perfil-image"
-            />
-            <div className='conteudo-popup'>
-              <h3>Complete o seu perfil</h3>
-              <p>Continue personalizando o seu perfil</p>
-            </div>
-          </div>  
-            <button className="btn-comecar">COMEÇAR</button>
-          </div>
-        </div>
-        
-        <MenuBar />
       </div>
+
+      <div className="perfil-options">
+        <PerfilOption icon={relogio} label="Order History" />
+        <PerfilOption icon={cadeado} label="Privacy Policy" />
+        <PerfilOption icon={config} label="Settings" />
+        <PerfilOption icon={logout} label="Log out" />
+      </div>
+
+      <MenuBar />
     </div>
   );
 }

56fb9135f6ff96bb5b5a78755306024ef5b7db4e
NataliaC2007 <nataliacostaaraujo5@gmail.com>
2024-11-06 10:50:09 -0300

    Mudança dos icons no app

--- src/components/MenuBar/MenuBar.jsx
+++ src/components/MenuBar/MenuBar.jsx
@@ -1,6 +1,11 @@
-  import React from 'react';
-  import { Link } from 'react-router-dom'; 
+import React from 'react';
+  import { Link } from 'react-router-dom';
   import '../MenuBar/MenuBar.css';
+  import homeIcon from '../../Assets/HomeIcon.png';
+  import rankingIcon from '../../Assets/RankingIcon.png';
+  import personalizacaoIcon from '../../Assets/PersonalizacaoIcon.png';
+  import perfilIcon from '../../Assets/PerfilIcon.png';
+
 
   const MenuBar = ({ activePage }) => {
     return (
@@ -9,22 +14,30 @@
           <ul className="nav-links">
             <li>
               <Link to="/HomePage" className={activePage === 'home' ? 'active' : ''}>
-              <i className="icon">🏠</i>
+              <i className="icon">
+                <img src={homeIcon} alt="Home icon" className="custom-icon" />
+              </i>
               </Link>
             </li>
             <li>
               <Link to="/Ranking" className={activePage === 'medalha' ? 'active' : ''}>
-              <i className="icon">🏅</i>
+              <i className="icon">
+                <img src={rankingIcon} alt="Ranking icon" className="custom-icon" />
+              </i>
               </Link>
             </li>
             <li>
               <Link to="/TelaPersonalizacao" className={activePage === 'pig' ? 'active' : ''}>
-              <i className="icon">🖌️</i>
+              <i className="icon">
+                <img src={personalizacaoIcon} alt="Personalização icon" className="custom-icon" />
+              </i>
               </Link>
             </li>
             <li>
               <Link to="/Perfil" className={activePage === 'perfil' ? 'active' : ''}>
-              <i className="icon">👤</i>
+              <i className="icon">
+                <img src={perfilIcon} alt="Perfil icon" className="custom-icon" />
+              </i>
               </Link>
             </li>
           </ul>
@@ -34,4 +47,3 @@
   };
 
   export default MenuBar;
-  

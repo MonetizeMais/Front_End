e63e62c0c046aedf5f5da3ca681087e348ee955e
Ana Clara Zampier Teixeira <anateixeira-ieg@instituto.ad>
2024-11-08 06:55:21 -0300

    teste

--- src/Screens/Ranking/Ranking.jsx
+++ src/Screens/Ranking/Ranking.jsx
@@ -2,7 +2,6 @@ import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import './Ranking.css'; 
 import pig from '../../Assets/Mascote 2.png'; 
-import medalha from '../../Assets/award-solid 1.png';   
 import MenuBar from '../../components/MenuBar/MenuBar';
 
 function Ranking() {
@@ -17,11 +16,10 @@ function Ranking() {
     } else {
       setLoggedInUserEmail(email);
 
-      // Buscar todos os usuários com suas fotos de perfil atualizadas
       axios.get('https://back-end-retz.onrender.com/getAllUsersByPoints')
         .then(response => {
           setUsers(response.data);
-          console.log(response.data);  // Assume que response.data inclui a URL da foto de perfil e outros dados
+          console.log(response.data);  
         })
         .catch(error => {
           console.error('Erro ao buscar os usuários:', error);  
@@ -33,7 +31,7 @@ function Ranking() {
     <div>
       <MenuBar />
       <h2 className="ranking-title">RANKING</h2>
-
+      <br />
       {users
         .filter(user => user.email === loggedInUserEmail)
         .map(user => (
@@ -59,18 +57,12 @@ function Ranking() {
               cardClass += " logged-user"; 
             }
 
-            // Usar a URL da foto de perfil do banco de dados, ou uma imagem padrão se não houver
             const userImage = user.fotoPerfil || pig;
 
             return (
               <div className={cardClass} key={user.email}>
                 <div className="user-photo">
                   <img src={userImage} alt="Foto do usuário" />
-                  {index === 0 && (
-                    <div className="medal-container">
-                      <img src={medalha} alt="Medalha" className="medalha" />
-                    </div>
-                  )}
                   {user.email === loggedInUserEmail && index !== 0 && (
                     <div className="tag-eu">Eu</div>
                   )}
